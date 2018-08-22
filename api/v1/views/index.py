ddcbe47272fabf857dc52b69e9af0ced238827ee
Lizzie Turner <hello@lizzieturner.com>
2018-08-22 22:32:19 +0000

    Add documentation and make executable

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,7 +1,13 @@
 #!/usr/bin/python3
-from api.v1.views.index import app_views
+""" returns json statuses for app_views routes  """
+from api.v1.views import app_views
 
-@app_views.route('/status')
+
+@app_views.route('/status', strict_slashes=False)
 def stat_return():
     """return json status: OK"""
     return jsonify({"status": "OK"})
+
+
+if __name__ == "__main__":
+    pass
